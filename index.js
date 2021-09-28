9c0d4b606e4b8abe6767cf824a135091655ba706
HaarikaR <hramadugu@gmail.com>
2021-09-28 13:19:03 -0700

    commit tree 8f5cb6633847156622bfe562a54277e0090c83ee

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+
+function superbowlWin(record){
+
+//console.log(record);
+    
+ let found = record.find(element => element.result === "W");
+
+ if(found === undefined){
+     return undefined;
+ }else{
+     return found.year;
+ }
+ 
+}
+
+//superbowlWin([{ year: "1978", result: "N/A"}]);
